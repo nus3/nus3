0b26b6ed6ec12910c7ffc5ff7a77ba365bc29e06
yota-hada <hadayota33@gmail.com>
2019-05-06 16:43:53 +0900

    temp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ const nuxtConfig: NuxtConfiguration = {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
   /*
    ** Axios module configuration
---
+++ pages/top.vue
@@ -0,0 +1,30 @@
+<template>
+    <main class="container">
+        <h1 class="title">hello</h1>
+    </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+})
+</script>
+
+<style lang="scss" scoped>
+$bgColor: #364046;
+$textColor: #e3f2fd;
+
+.container {
+    width: 100vw;
+    height: 100vh;
+    background-color: $bgColor;
+
+    .title {
+        margin: 0;
+        color: $textColor;
+        font-size: 50px;
+        margin-bottom: 20px;
+    }
+}
+</style>
